ef996553bcecd26deaa43e426729c84322250ee9
Maciej Orliński <maciej.orlinski@daftmobile.com>
2022-06-09 15:53:23 +0200

    Second batch, main menu added

---
+++ Runtime/Scripts/MainMenu/AudioController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+	[Header("References:")]
+	[SerializeField] private Image targetImage = null;
+
+	[SerializeField] private Sprite soundOnLoud = null;
+	[SerializeField] private Sprite soundOn = null;
+	[SerializeField] private Sprite soundOff = null;
+	[SerializeField] private AudioMixerGroup masterGroup = null;
+	[SerializeField] private Slider slider = null;
+
+	[Header("Parameters")]
+	[SerializeField] private int mindB = -80;
+	[SerializeField] private int maxdB = 20;
+	[SerializeField] private AnimationCurve volumeScaleCurve = null;
+
+	private const string MASTER_VOLUME = "MasterVolume";
+
+	private float dBValueRange = 0;
+	private float lastValue = 50;
+
+	private void Awake()
+	{
+		dBValueRange = Mathf.Abs(mindB) + Mathf.Abs(maxdB);
+
+		UpdateAudioDB(50);
+	}
+
+	public void UpdateAudioDB(float value0100)
+	{
+		value0100 = Mathf.Round(value0100);
+
+		float newVolume = mindB + (dBValueRange * volumeScaleCurve.Evaluate(value0100 / 100.0f));
+		masterGroup.audioMixer.SetFloat(MASTER_VOLUME, newVolume);
+		SetSoundSprite(value0100);
+
+		slider.value = value0100;
+	}
+
+	private void SetSoundSprite(float value0100)
+	{
+		if (value0100 > 70f)
+			targetImage.sprite = soundOnLoud;
+		else if (value0100 < 1.0f)
+			targetImage.sprite = soundOff;
+		else
+			targetImage.sprite = soundOn;
+	}
+
+	public void ToggleAudio()
+	{
+		masterGroup.audioMixer.GetFloat(MASTER_VOLUME, out float currentVolume);
+
+		if (currentVolume == mindB)
+		{
+			UpdateAudioDB(lastValue);
+		}
+		else
+		{
+			lastValue = slider.value;
+			UpdateAudioDB(0);
+		}
+	}
+}
---
+++ Runtime/Scripts/MainMenu/CoinsUpdater.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UniRx;
+using Zenject;
+using DG.Tweening;
+
+public class CoinsUpdater : MonoBehaviour
+{
+	[Header("Token exchange website url")]
+	[SerializeField] private string url = "https://app.sushi.com/swap?tokens=MATIC&tokens=0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063&chainId=137";
+
+	[Header("Scene References")]
+	[SerializeField] private TextMeshProUGUI coinsBalance = null;
+	[SerializeField] private float refreshingCoinStatusInSeconds = 3;
+	private SCController controller;
+
+	[Inject]
+	private void Inject(SCController controller)
+	{
+		this.controller = controller;
+	}
+	private void Start()
+	{
+		controller.Model.TokenBalance.Subscribe(tokenBalance =>
+		{
+			coinsBalance.text = controller.Model.TokenBalance.ToString();
+			coinsBalance.transform.DOShakeScale(0.1f, 0.2f);
+		});
+		StartCoroutine(AutomaticRefresherWalletBalance());
+	}
+
+	public void OpenExternalTokenWebsite()
+	{
+		Application.OpenURL(url);
+	}
+
+	private IEnumerator AutomaticRefresherWalletBalance()
+	{
+		var wait = new WaitForSeconds(refreshingCoinStatusInSeconds);
+		while (true)
+		{
+			yield return wait;
+			yield return controller.GetTokenBalance(null);
+		}
+	}
+}
