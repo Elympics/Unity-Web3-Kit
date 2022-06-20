d5b0caa59b596365fb640ea0cfc0be7e8cd60a56
Maciej Orliński <maciej.orlinski@daftmobile.com>
2022-06-20 17:11:00 +0200

    Showing first time setup automatically

--- Editor/FirstTimeSetup.cs
+++ Editor/FirstTimeSetup.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEditor;
-using System.IO;
 using Elympics;
 
 namespace Web3Kit
@@ -15,10 +14,20 @@ namespace Web3Kit
 		[MenuItem("Tools/Web3Kit/First time setup")]
 		public static void Init()
 		{
+			var window = GetWindow<FirstTimeSetup>("Web3Kit Set Up");
+			window.Show();
+		}
+
+		private void RunSetup()
+		{
+			bool success = true;
 			var config = Resources.Load<FirstTimeSetupConfig>("FirstTimeSetupConfig");
 			var path = AssetDatabase.GetAssetPath(config.FolderToCopy);
 			if (!AssetDatabase.CopyAsset(path, DestinationPath))
+			{
 				Debug.LogError("[Web3Kit:FirstTimeSetup] Copying files failed!");
+				success = false;
+			}
 
 			AssetDatabase.ImportAsset(DestinationPath);
 			var error = AssetDatabase.RenameAsset(DestinationPath + "/_Resources", "Resources");
@@ -26,14 +35,15 @@ namespace Web3Kit
 			{
 				Debug.LogError(error);
 				Debug.LogError("[Web3Kit:FirstTimeSetup] Renaming _Resources to Resources failed!");
+				success = false;
 			}
 
 			ReassignReferences();
 
-			var window = GetWindow<FirstTimeSetup>();
-			window.roomApiConfig = Resources.Load<ElympicsRoomAPIConfig>(ElympicsRoomAPIConfig.PATH_IN_RESOURCES);
-			window.smartContractConfig = Resources.Load<SmartContractConfig>(SmartContractConfig.PATH_IN_RESOURCES);
-			window.Show();
+			FirstTimeSetupLauncher.MarkFirstTimeSetupRun(success);
+
+			roomApiConfig = Resources.Load<ElympicsRoomAPIConfig>(ElympicsRoomAPIConfig.PATH_IN_RESOURCES);
+			smartContractConfig = Resources.Load<SmartContractConfig>(SmartContractConfig.PATH_IN_RESOURCES);
 		}
 
 		[MenuItem("Tools/Web3Kit/Reassign references")]
@@ -49,29 +59,41 @@ namespace Web3Kit
 		private void OnGUI()
 		{
 			GUILayout.Label("Welcome to Elympics Web3Kit!", EditorStyles.largeLabel);
-			GUILayout.Label("This setup script has copied files to " + DestinationPath + ". " +
-				"Check contents of that folder and edit anything you like, but preserve the directory structure.", EditorStyles.wordWrappedLabel);
-			GUILayout.Label("Let's run a quick setup, you can do this manually by editing scriptable objects in the Web3Kit folder. " +
-				"If you don't know what to set for something, just leave it empty and come back later.", EditorStyles.wordWrappedLabel);
 
-			roomApiConfig.Uri = EditorGUILayout.TextField("Elympics Room API uri", roomApiConfig.Uri);
-			AssetDatabase.SaveAssetIfDirty(roomApiConfig);
-
-			smartContractConfig.useSmartContract = EditorGUILayout.Toggle("Use blockchain integration?", smartContractConfig.useSmartContract);
-			if (smartContractConfig.useSmartContract)
+			if (!FirstTimeSetupLauncher.DidRunFirstTimeSetup())
 			{
-				GUILayout.Label("New to web3? You can set up this section later and use your game in play for free mode.", EditorStyles.wordWrappedLabel);
-				smartContractConfig.smartContractAddress = EditorGUILayout.TextField("Smart contract address", smartContractConfig.smartContractAddress);
-				smartContractConfig.chainAddress = EditorGUILayout.TextField("Chain address", smartContractConfig.chainAddress);
+				GUILayout.Label("Run first time setup to copy and setup necessary files. This will only take a few seconds.", EditorStyles.wordWrappedLabel);
+				if (GUILayout.Button("Start"))
+					RunSetup();
+				bool skip = GUILayout.Toggle(FirstTimeSetupLauncher.ShouldSkipFirstTimeSetup(), "Don't ask again");
+				FirstTimeSetupLauncher.ToggleSkipFirstTimeSetup(skip);
 			}
-			AssetDatabase.SaveAssetIfDirty(smartContractConfig);
+			else
+			{
+				GUILayout.Label("This setup script has copied files to " + DestinationPath + ". " +
+					"Check contents of that folder and edit anything you like, but preserve the directory structure.", EditorStyles.wordWrappedLabel);
+				GUILayout.Label("Let's run a quick setup, you can do this manually by editing scriptable objects in the Web3Kit folder. " +
+					"If you don't know what to set for something, just leave it empty and come back later.", EditorStyles.wordWrappedLabel);
 
-			GUILayout.Label("Next step: open your elympics config and create a new game by clicking the button below or using Tools/Elympics/Manage games in Elympics.", EditorStyles.wordWrappedLabel);
-			if (GUILayout.Button("Manage games in Elympics"))
-				OpenManageGamesInElympicsWindow();
-			
-			GUILayout.Label("Drag and drop TitleScreenController and MainMenuController from " + DestinationPath + "/Prefabs/ to your scenes " +
-				"or start with " + DestinationPath + "/Scenes/. Remember to add them to build settings!", EditorStyles.wordWrappedLabel);
+				roomApiConfig.Uri = EditorGUILayout.TextField("Elympics Room API uri", roomApiConfig.Uri);
+				AssetDatabase.SaveAssetIfDirty(roomApiConfig);
+
+				smartContractConfig.useSmartContract = EditorGUILayout.Toggle("Use blockchain integration?", smartContractConfig.useSmartContract);
+				if (smartContractConfig.useSmartContract)
+				{
+					GUILayout.Label("New to web3? You can set up this section later and use your game in play for free mode.", EditorStyles.wordWrappedLabel);
+					smartContractConfig.smartContractAddress = EditorGUILayout.TextField("Smart contract address", smartContractConfig.smartContractAddress);
+					smartContractConfig.chainAddress = EditorGUILayout.TextField("Chain address", smartContractConfig.chainAddress);
+				}
+				AssetDatabase.SaveAssetIfDirty(smartContractConfig);
+
+				GUILayout.Label("Next step: open your elympics config and create a new game by clicking the button below or using Tools/Elympics/Manage games in Elympics.", EditorStyles.wordWrappedLabel);
+				if (GUILayout.Button("Manage games in Elympics"))
+					OpenManageGamesInElympicsWindow();
+
+				GUILayout.Label("Drag and drop TitleScreenController and MainMenuController from " + DestinationPath + "/Prefabs/ to your scenes " +
+					"or start with " + DestinationPath + "/Scenes/. Remember to add them to build settings!", EditorStyles.wordWrappedLabel);
+			}
 		}
 
 		// TODO: make OpenManageGamesInElympicsWindow public in Elympic SDK
---
+++ Editor/FirstTimeSetupLauncher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace Web3Kit
+{
+	public class FirstTimeSetupLauncher
+	{
+		private const string SETUP_EDITOR_PREFS_KEY = "Web3KitFirstTimeSetup";
+		private const string SKIP_EDITOR_PREFS_KEY = "Web3KitSkipSetup";
+
+		public static bool DidRunFirstTimeSetup() => EditorPrefs.GetBool(SETUP_EDITOR_PREFS_KEY);
+
+		public static void MarkFirstTimeSetupRun(bool value) => EditorPrefs.SetBool(SETUP_EDITOR_PREFS_KEY, value);
+
+		public static bool ShouldSkipFirstTimeSetup() => EditorPrefs.GetBool(SKIP_EDITOR_PREFS_KEY);
+
+		public static void ToggleSkipFirstTimeSetup(bool value) => EditorPrefs.SetBool(SKIP_EDITOR_PREFS_KEY, value);
+
+		[DidReloadScripts]
+		private static void Launch()
+		{
+			if (!ShouldSkipFirstTimeSetup() && !DidRunFirstTimeSetup())
+				FirstTimeSetup.Init();
+		}
+	}
+}
